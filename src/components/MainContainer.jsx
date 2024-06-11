23d36c45d8f24209b2aa9d7ab628e4e8c7cdfa23
Hemant <hemant.agarwal8620@gmail.com>
2024-06-11 17:12:32 +0530

    corrections

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -5,18 +5,17 @@ import VideoTitle from "./VideoTitle";
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
 
-  if (!movies || !Array.isArray(movies) || movies.length === 0) return;
+  if (!movies) return;
 
-  const randomIndex = Math.floor(Math.random() * movies.length);
-  const mainMovie = movies[randomIndex];
+  const mainMovie = movies[0];
+
   const { original_title, overview, id } = mainMovie;
 
   return (
-    <div>
+    <div className="pt-[30%] bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
       <VideoBackground movieId={id} />
     </div>
   );
 };
-
 export default MainContainer;
