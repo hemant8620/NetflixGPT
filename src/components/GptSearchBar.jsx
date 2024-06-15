875977df8b838c45eb1737f8d4a265fd40a94c38
Hemant <hemant.agarwal8620@gmail.com>
2024-06-15 21:40:31 +0530

    suggestions

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,19 +1,58 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS, GOOGLE_KEY } from "../utils/constants";
 import lang from "../utils/language";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { gptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
   const language = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+  const genAI = new GoogleGenerativeAI(GOOGLE_KEY);
+  const dispatch = useDispatch();
+
+  const searchMovieTmdb = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+
+  const handleGptSearchClick = async () => {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const query =
+      "Act as a Movie Recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". Only give me names of 5 Movies , comma seperated like the example result given ahead. Example Result: Veeram, Mangatha, Jilla, Viswasam";
+    const result = await model.generateContent(query);
+    const response = await result.response;
+    const text = await response.text();
+    const movies = text.split(", ");
+    const promise = movies.map((movie) => searchMovieTmdb(movie));
+    const bardResults = await Promise.all(promise);
+    dispatch(gptMovieResult({ movieNames: movies, movieResults: bardResults }));
+  };
 
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 grid grid-cols-12">
+      <form
+        className="w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[language].placeHolderGptSearch}
         />
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
           {lang[language].search}
         </button>
       </form>
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -28,3 +28,7 @@ export const SUPPORTED_LANGUAGES = [
     name: "Spanish",
   },
 ];
+
+export const OPENAI_KEY =
+  "sk-proj-U1OdeSBJGcT24KfRbhK9T3BlbkFJmHhL1rVilcWDLTmn1i64";
+export const GOOGLE_KEY = "AIzaSyA5olKZIZjF9tNSuiDucSM_YYIu0DK-MGA";
